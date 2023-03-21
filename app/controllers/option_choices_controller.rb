66fcb4c377e23e7bff02e9303545fd73e7f54f57
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-03-21 17:38:00 -0300

    end of the day

---
+++ app/controllers/option_choices_controller.rb
@@ -0,0 +1,2 @@
+class OptionChoicesController < ApplicationController
+end
---
+++ test/controllers/option_choices_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class OptionChoicesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
