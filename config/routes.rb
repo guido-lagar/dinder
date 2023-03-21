852ecc904c547d80f8b87af071d196de4119edab
Guido Lagar <guidolagar@gmail.com>
2023-03-21 17:40:47 -0300

    21st march end

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'options/show'
+  get 'login/create', to: 'logins#create', as: :create_login
   devise_for :users
   root to: "pages#home"
 
   resources :options
   resources :choices, only: %i[index show]
+
+
 end
