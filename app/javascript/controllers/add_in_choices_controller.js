08e1301ded8b2cabfbd8f7ea1e4750c71918a92b
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-03-22 18:15:28 -0300

    new font

--- app/javascript/controllers/add_in_choices_controller.js
+++ app/javascript/controllers/add_in_choices_controller.js
@@ -6,30 +6,16 @@ export default class extends Controller {
   connect() {
 
   }
-  // async refreshContent(event) {
-  //   const target = this.hasContentTarget ? this.contentTarget : this.element;
-  //   target.style.opacity = .5;
-  //   const response = await fetch(this.urlValue);
-  //   target.innerHTML = await response.text();
-  //   target.style.opacity = 1;
-}
+  initialize(event){
+    event.clickCount = 0
+  }
+
   add(event) {
-    // new_option = event.target.dataset.optionId
+    event.clickCount +=1
     console.log(event.target.dataset.optionId)
+    console.log(this.clickCount)
+    
+    window.location.reload();
 
-
-    // fetch(this.choiceTarget.action, {
-    //   method: "POST",
-    //   headers: { "Accept": "application/json" },
-    //   body: new FormData(this.choiceTarget)
-    // })
-    //   .then(response => response.json())
-    //   .then((data) => {
-    //     console.log(data)
-    //   })
-
-    // new_option_id = event.target.dataset.optionId
-    // optionchoice = OptionChoice.new(choice_id: @choice.id, option_id: new_option_id)
-    // optionchoice.save
   }
 }
