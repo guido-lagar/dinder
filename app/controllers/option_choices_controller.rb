6b3fc99749cbc825ba49a2ffd244094c7324ef93
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-03-22 14:21:53 -0300

    save the options for choice

--- app/controllers/option_choices_controller.rb
+++ app/controllers/option_choices_controller.rb
@@ -1,6 +1,7 @@
 class OptionChoicesController < ApplicationController
-  
+
   def create
-  @optionchoice = OptionChoice.new(choice_id: @choice.id, option_id: params[:event.target.dataset.optionId])
+  @optionchoice = OptionChoice.new(choice_id: params[:choice_id], option_id: params[:option_id])
+  @optionchoice.save
   end
 end
