95ba8f0050ea4544dac99ec69fe5ac696cbc59d5
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-03-22 11:33:17 -0300

    working schema

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,5 @@
 class Option < ApplicationRecord
   has_one_attached :photo
+  has_many :option_choices
   has_many :choices, through: :option_choices
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
