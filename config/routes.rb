392dede938332c2448ebd339ba5fecc66b1cb01d
Shahtaj Anwar <shahtaj.anwar@gmail.com>
2023-03-28 17:14:17 -0300

    Log in, sign up and settings

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,16 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
-  get '/settings', to: 'settings#edit'
-  patch '/settings', to: 'settings#update'
+  get 'settings', to: 'settings#index'
+  put 'settings', to: 'settings#update'
 
   resources :choices, only: %i[index show create new edit] do
     resources :receiptors
     resources :options do
-    resources :option_choices, only: :create
+      resources :option_choices, only: :create
+    end
   end
-end
 
 end
+
+# Path: app/controllers/settings_controller.rb
