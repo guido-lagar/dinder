9b6d3115b43569a68d820ef86f2175d5c576f93c
Guido Lagar <guidolagar@gmail.com>
2023-03-30 12:41:18 -0300

    revert

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,5 +1,5 @@
 class Option < ApplicationRecord
   has_one_attached :photo
-  has_many :option_choices, foreign_key: :option_id
+  has_many :option_choices
   has_many :choices, through: :option_choices
 end
