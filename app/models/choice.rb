1a962393be58decad50b10ed242a163c5b34b070
Guido Lagar <guidolagar@gmail.com>
2023-03-30 12:10:12 -0300

    replace-option-ac-record-association

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,6 +1,9 @@
 class Choice < ApplicationRecord
   belongs_to :user
   has_many :option_choices
-  has_many :options, through: :option_choices
   has_many :receiptors
+
+  def options
+    Option.where(id: OptionChoice.where(choice: self).pluck(:option_id))
+  end
 end
