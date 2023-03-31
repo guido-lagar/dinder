2a4f1055844d8c57cfb9cb8aa2aa36d3db9e787e
Guido Lagar <guidolagar@gmail.com>
2023-03-31 14:33:17 -0300

    destroy all seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ require "open-uri"
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Receiptor.destroy_all
 OptionChoice.destroy_all
 Choice.destroy_all
 Option.destroy_all
